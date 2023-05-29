3e201613ae6ffd512bb47a5de9d3d53f8cba23f0
jPabloGomez1913 <Juangomezramirez527@gmail.com>
2023-05-29 12:11:32 -0500

    BooksController added

--- LibraryProyect/LibraryProyect/Controllers/BooksController.cs
+++ LibraryProyect/LibraryProyect/Controllers/BooksController.cs
@@ -69,5 +69,50 @@ namespace LibraryProyect.Controllers
 
             return Ok(book);
         }
+
+
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit/{id}")]
+        public async Task<ActionResult> EditBook(Guid? id, Book book)
+        {
+            try
+            {
+                if (id != book.id) return NotFound("book not found");
+
+
+
+                _context.Books.Update(book);
+                await _context.SaveChangesAsync(); // Aquí es donde se hace el Update...
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", book.id));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(book);
+        }
+
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete/{id}")]
+        public async Task<ActionResult> DeleteBook(Guid? id)
+        {
+            if (_context.Books == null) return Problem("Entity set 'DataBaseContext.Books' is null.");
+            var book = await _context.Books.FirstOrDefaultAsync(c => c.id == id);
+
+            if (book == null) return NotFound("student not found");
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync(); //Hace las veces del Delete en SQL
+
+            return Ok(String.Format("El libro fue eliminado!"));
+        }
+
     }
 }
--- LibraryProyect/LibraryProyect/DAL/Entities/Book.cs
+++ LibraryProyect/LibraryProyect/DAL/Entities/Book.cs
@@ -8,7 +8,7 @@ namespace LibraryProyect.DAL.Entities
         public Guid id { get; set; }
 
         [Display (Name="Nombre del libro:")]
-        [MaxLength(30, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [MaxLength(60, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string NombreLibro { get; set; }
         [Display(Name = "Autor del libro:")]
