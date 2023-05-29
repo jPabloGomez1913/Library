7eb91af18e8a51cf3010550632dddbcae80651e3
jPabloGomez1913 <Juangomezramirez527@gmail.com>
2023-05-28 19:17:27 -0500

    Migration created

--- LibraryProyect/LibraryProyect/DAL/DataBaseContext.cs
+++ LibraryProyect/LibraryProyect/DAL/DataBaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibraryProyect.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryProyect.DAL
 {
@@ -9,5 +10,25 @@ namespace LibraryProyect.DAL
         {
 
         }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+
+            base.OnModelCreating(modelBuilder);
+            //Creacion del indice del estudiante
+            modelBuilder.Entity<Student>().HasIndex(s =>s.StudentCode).IsUnique();
+            //Creacion del indice del empleado
+            modelBuilder.Entity<Employee>().HasIndex(s =>s.EmployeeCode).IsUnique();
+            //Creacion del indice del docente
+            modelBuilder.Entity<Teacher>().HasIndex(s =>s.TeacherCode).IsUnique();
+        }
+
+
+
+
     }
 }
---
+++ LibraryProyect/LibraryProyect/Migrations/20230529000924_InitialDataBase.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryProyect.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NombreLibro = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    AutorLibro = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TeacherCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_EmployeeCode",
+                table: "Employees",
+                column: "EmployeeCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_StudentCode",
+                table: "Students",
+                column: "StudentCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_TeacherCode",
+                table: "Teachers",
+                column: "TeacherCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
